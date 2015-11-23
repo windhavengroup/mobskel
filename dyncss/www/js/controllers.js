f904caaccc97c30c41b5efbe0a03fd689544c637
Dennis Parker <dennis.parker@gmail.com>
2015-11-23 15:36:14 -0600

    Added some content using ionic ui components, tab and side menu nav

--- dyncss/www/js/controllers.js
+++ dyncss/www/js/controllers.js
@@ -1,28 +1,32 @@
-angular.module('starter.controllers', [])
+angular.module('app.controllers', [])
+     
+.controller('pictureCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.go = function ( path ) {
+	$location.path( path );
+    };
+}])
+.controller('signupCtrl', function($scope) {
 
-.controller('DashCtrl', function($scope) {})
+})
+.controller('menuCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.go = function ( path ) {
+	$location.path( path );
+    };
+}])
+.controller('loginCtrl', function($scope) {
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+})
+      
+.controller('cardCtrl', function($scope) {
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+})
+   
+.controller('listCtrl', function($scope) {
+
+})
+   
+.controller('textCtrl', function($scope) {
+
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+ 
